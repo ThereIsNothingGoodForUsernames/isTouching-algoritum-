9d35b55d702b2e72ba2385fd620dccbc94496fd7
ThereIsNothingGoodForUsernames <64938860+ThereIsNothingGoodForUsernames@users.noreply.github.com>
2020-05-13 19:06:22 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var fixedRect, movingRect
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 30, 80);
+  movingRect = createSprite(600, 200, 80, 30);
+  fixedRect.shapeColor = "blue";
+  movingRect.shapeColor = "blue";
+}
+
+function draw() {
+  background(0);  
+  
+  movingRect.x = mouseX
+  movingRect.y = mouseY
+  
+  if(movingRect.width/2 + fixedRect.width/2 > movingRect.x - fixedRect.x && movingRect.width/2 + fixedRect.width/2 > fixedRect.x - movingRect.x  && movingRect.height/2 + fixedRect.height/2 > movingRect.y - fixedRect.y && movingRect.height/2 + fixedRect.height/2 > fixedRect.y - movingRect.y)  {
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  else  {
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "blue";
+  }
+  drawSprites(); 
+}
